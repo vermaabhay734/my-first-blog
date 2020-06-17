b6f6f58a59f003bcfa9ee6635353a35c6e038937
acer <vermaabhay734@gmail.com>
2020-06-17 15:53:09 +0530

    Modified templates to display posts from database.

--- blog/admin.py
+++ blog/admin.py
@@ -2,3 +2,8 @@ from django.contrib import admin
 from .models import Post
 # Register your models here.
 admin.site.register(Post)
+
+'''
+from .models import Product
+admin.site.register(Product)
+'''
