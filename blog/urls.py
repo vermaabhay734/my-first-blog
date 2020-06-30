5722df71766185026902b3d89c236258733b69bb
abhay <vermaabhay734@gmail.com>
2020-06-30 23:52:18 +0530

    nothing

--- blog/urls.py
+++ blog/urls.py
@@ -13,5 +13,5 @@ urlpatterns = [
     path('post/<pk>/remove/', views.post_remove, name='post_remove'),
     path('post/<int:pk>/comment/', views.add_comment_to_post, name='add_comment_to_post'),
     path('comment/<int:pk>/approve/', views.comment_approve, name='comment_approve'),
-path('comment/<int:pk>/remove/', views.comment_remove, name='comment_remove'),
+    path('comment/<int:pk>/remove/', views.comment_remove, name='comment_remove'),
 ]
